beee249961cf92748785f6689df6d542a2ed13b2
TaeYoung Kim <falcon@yonsei.ac.kr>
2020-08-20 17:09:04 +0900

    08/20 pakagename change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     # 배포할 패키지의 이름을 적어줍니다. setup.py파일을 가지는 폴더 이름과 동일하게 합니다.
-    name                = 'oisl-util',
+    name                = 'oislutil',
     # 배포할 패키지의 버전을 적어줍니다. 첫 등록이므로 0.1 또는 0.0.1을 사용합니다.
     version             = '0.0.1',
     # 배포할 패키지에 대한 설명을 작성합니다.
